4dd853e8c709b6008f82c4b37e9771197245ee6a
Nizaxga <nicekungc57@gmail.com>
2025-09-01 21:07:42 +0700

    [FEA] Compare diff model.

--- Scripts/sentence_transformer_load.py
+++ Scripts/sentence_transformer_load.py
@@ -16,30 +16,29 @@ embedded_2 -= np.mean(embedded_2, axis=0, keepdims=True)
 
 embedded_concat = np.vstack([embedded_1, embedded_2])
 
-
-U1, S1, VT1 = np.linalg.svd(embedded_1, full_matrices=False)
-V1 = VT1.T
-U2, S2, VT2 = np.linalg.svd(embedded_2, full_matrices=False)
-V2 = VT2.T
+# U1, S1, VT1 = np.linalg.svd(embedded_1, full_matrices=False)
+# V1 = VT1.T
+# U2, S2, VT2 = np.linalg.svd(embedded_2, full_matrices=False)
+# V2 = VT2.T
 
 Uconcat, Sconcat, VTconcat = np.linalg.svd(embedded_concat, full_matrices=False)
 Vconcat = VTconcat.T
 
-g_x = [V1[0] @ embedded_1[i].T for i in range(len(embedded_1))]
-g_y = [V1[1] @ embedded_1[i].T for i in range(len(embedded_1))]
+# g_x = [V1[0] @ embedded_1[i].T for i in range(len(embedded_1))]
+# g_y = [V1[1] @ embedded_1[i].T for i in range(len(embedded_1))]
 
-plt.figure()
-plt.title("embedded1: basis V[0] V[1]")
-plt.scatter(g_x, g_y, alpha=0.5)
-plt.savefig("outputs/svd_bank77.png")
+# plt.figure()
+# plt.title("embedded1: basis V[0] V[1]")
+# plt.scatter(g_x, g_y, alpha=0.5)
+# plt.savefig("outputs/svd_bank77.png")
 
-g_x = [V2[0] @ embedded_2[i].T for i in range(len(embedded_2))]
-g_y = [V2[1] @ embedded_2[i].T for i in range(len(embedded_2))]
+# g_x = [V2[0] @ embedded_2[i].T for i in range(len(embedded_2))]
+# g_y = [V2[1] @ embedded_2[i].T for i in range(len(embedded_2))]
 
-plt.figure()
-plt.title("embedded2: basis V[0] V[1]")
-plt.scatter(g_x, g_y, alpha=0.5)
-plt.savefig("outputs/svd_stsbench.png")
+# plt.figure()
+# plt.title("embedded2: basis V[0] V[1]")
+# plt.scatter(g_x, g_y, alpha=0.5)
+# plt.savefig("outputs/svd_stsbench.png")
 
 # g_x = [Vconcat[0] @ embedded_concat[i].T for i in range(len(embedded_concat))]
 # g_y = [Vconcat[1] @ embedded_concat[i].T for i in range(len(embedded_concat))]
